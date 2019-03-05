7ca112ffd64c8b509fa15f93325feeb3824e81f7
Sebastian Golian <sebastiangolian@gmail.com>
2019-03-05 19:35:17 +0100

    update test componet

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -5,7 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
+
 export class TestComponent implements OnInit {
+
+  title = 'My heroes';
+  heroes = ['Rambo', 'Terminator', 'Rocky', 'Superman'];
+  defaultHero = this.heroes[0];
 
   constructor() { }
 
