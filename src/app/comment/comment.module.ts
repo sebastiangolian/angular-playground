ad97332834dda543cc35669059c3798feddfc9c9
Sebastian Golian <sebastiangolian@gmail.com>
2019-03-28 19:46:43 +0100

    add comment-item component

--- src/app/comment/comment.module.ts
+++ src/app/comment/comment.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentByPostComponent } from './components/comment-by-post/comment-by-post.component';
+import { CommentItemComponent } from './components/comment-item/comment-item.component';
 
 @NgModule({
-  declarations: [CommentByPostComponent],
+  declarations: [CommentByPostComponent, CommentItemComponent],
   imports: [
     CommonModule
   ],
