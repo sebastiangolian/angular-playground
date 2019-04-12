063078fc2d5a431566f22762c0510f31095fc1e2
Sebastian Golian <sebastiangolian@gmail.com>
2019-04-12 20:26:48 +0200

    add popper.js

--- src/app/post/services/post.service.spec.ts
+++ src/app/post/services/post.service.spec.ts
@@ -1,17 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PostService } from './post.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 
 describe('PostService', () => {
-  beforeEach(() => TestBed.configureTestingModule({ 
-    imports: [
-      HttpClientModule
-    ]
-  }));
+
+  let service: PostService;
+  let httpClientSpy: { get: jasmine.Spy };
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new PostService(<any> httpClientSpy);
+    TestBed.configureTestingModule({ imports: [HttpClientModule]})
+  });
 
   it('should be created', () => {
     const service: PostService = TestBed.get(PostService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return an error when the server returns a 404', () => {
+    // const errorResponse = new HttpErrorResponse({
+    //   error: 'test 404 error',
+    //   status: 404, statusText: 'Not Found'
+    // });
+   
+    //httpClientSpy.get.and.returnValue(errorResponse);
+    console.log(service.get("1"));
+    // service.get("1").subscribe(
+    //   heroes => fail('expected an error, not heroes'),
+    //   error  => expect(error.message).toContain('test 404 error')
+    // );
   });
 });
