bc02d68d3fd7a317e02c49b7c2a22937d3057f8e
Sebastian Golian <sebastiangolian@gmail.com>
2019-04-26 14:37:53 +0200

    create template driven form

--- src/app/profile/components/profile-form-template-driven/profile-form-template-driven.component.ts
+++ src/app/profile/components/profile-form-template-driven/profile-form-template-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-form-template-driven',
@@ -8,50 +8,18 @@ import { FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
 })
 export class ProfileFormTemplateDrivenComponent {
 
-  profileForm: FormGroup = this.fb.group({
-    firstName: ['',[Validators.required,Validators.minLength(3), Validators.maxLength(25)]],
-    lastName: ['',[Validators.required,Validators.minLength(3), Validators.maxLength(25)]],
-    address: this.fb.group({
-      street: ['',Validators.required],
-      city: ['',Validators.required],
-      state: ['',Validators.required],
-      zip: ['',Validators.required]
-    }),
-    aliases: this.fb.array([
-      this.fb.control('')
-    ])
-  });
-
-  constructor(private fb: FormBuilder) { }
-
-  get aliases(): FormArray {
-    return this.profileForm.get('aliases') as FormArray;
-  }
-
-  get f() { return this.profileForm.controls; }
-
-  public addAlias(): void {
-    this.aliases.push(this.fb.control(''));
-  }
+  model: any = {};
+  aliases: Array<String> = new Array<String>();
 
   public addDefault(): void {
-    this.profileForm.patchValue({
-      firstName: 'Jan',
-      lastName: 'Kowalski',
-      address: {
-        street: 'Testowa 2',
-        city: 'Testowo',
-        state: 'Polska',
-        zip: '00-000'
-      },
-      aliases: [
-        'alias 1'
-      ]
-    });
+    this.model.login = 'testlogin';
+    this.model.email = 'test@email.com';
+    this.model.city = 'test';
+    this.model.zipcode =  '00-000';
   }
 
-  public onSubmit():void  {
-    console.log(this.profileForm.value);
+  onSubmit(f:NgForm) {
+    console.log(this.model);
+    console.log(f.value);
   }
-
 }
