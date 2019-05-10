72bfd8b64b6cf4ddd77608b89ae75d04ce1a1335
Sebastian Golian <sebastiangolian@gmail.com>
2019-05-10 13:27:33 +0200

    update game-api component test

--- src/app/game/components/game-api/game-api.component.spec.ts
+++ src/app/game/components/game-api/game-api.component.spec.ts
@@ -1,27 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { GameApiComponent } from './game-api.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { GameApiService } from '../../services/game-api.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
 
 describe('GameApiComponent', () => {
   let component: GameApiComponent;
   let fixture: ComponentFixture<GameApiComponent>;
+  let httpBackend: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ 
-        GameApiComponent 
+      providers: [
+        GameApiService
+      ],
+      declarations: [
+        GameApiComponent
       ],
       imports: [
         ReactiveFormsModule,
         FormsModule,
         HttpClientModule,
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -30,7 +38,38 @@ describe('GameApiComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(inject([HttpTestingController], (backend: HttpTestingController) => {
+    httpBackend = backend;
+    fixture = TestBed.createComponent(GameApiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    // httpBackend
+    //   .expectOne({ url: 'http://localhost:3000/api/games/', method: 'GET' }, 'Get list of games')
+    //   .flush([
+    //     {
+    //       "id": 1,
+    //       "title": "FIFA",
+    //       "price": 100
+    //     },
+    //     {
+    //       "id": 2,
+    //       "title": "PES",
+    //       "price": 150
+    //     },
+    //     {
+    //       "id": 3,
+    //       "title": "WOT",
+    //       "price": 0
+    //     }
+    //   ]);
+  }));
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    httpBackend.verify();
   });
 });
