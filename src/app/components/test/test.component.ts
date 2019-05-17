8df291cef5c97d1cd809dceb679e82b2c2f7e335
Sebastian Golian <sebastiangolian@gmail.com>
2019-05-17 10:44:19 +0200

    add products data to test component

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'src/app/services/message.service';
+import { Product } from 'src/app/interfaces/product.interface';
+import { Products } from 'src/app/data/products';
 
 @Component({
   selector: 'app-test',
@@ -9,6 +11,8 @@ import { MessageService } from 'src/app/services/message.service';
 
 export class TestComponent implements OnInit {
 
+  private products: Array<Product> = Products;
+
   constructor(public messageService: MessageService) {
     this.messageService.message = "Welcome in app-test component."
   }
@@ -16,5 +20,4 @@ export class TestComponent implements OnInit {
   ngOnInit() {
     
   }
-
 }
