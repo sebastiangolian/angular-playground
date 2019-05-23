a30c6fd6966ccbe7ff710c5881a20e67ac104894
sebastiangolian <sebastiangolian@gmail.com>
2019-05-23 09:20:17 +0200

    add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -17,6 +18,7 @@ import { TestComponent } from './components/test/test.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
