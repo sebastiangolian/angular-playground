0f46e90c20ef71e35826e3e4d9091399c52fa143
Sebastian Golian <sebastiangolian@gmail.com>
2019-08-27 14:54:39 +0200

    move app module to dir

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -6,10 +6,11 @@ import { PageHomeComponent } from './components/page-home/page-home.component';
 import { PageTestComponent } from './components/page-test/page-test.component';
 import { PageFormComponent } from './components/page-form/page-form.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SharedModule } from './shared/shared.module';
-import { MovieModule } from './movie/movie.module';
+import { SharedModule } from '../shared/shared.module';
+import { MovieModule } from '../movie/movie.module';
 import { FormsModule } from '@angular/forms';
-import { GlobalErrorHandlerService } from './shared/services/global-error-handler.service';
+import { GlobalErrorHandlerService } from '../shared/services/global-error-handler.service';
+
 
 @NgModule({
   declarations: [
