f4443ea58af379559a3c34a89b0575491c56aaec
Sebastian Golian <sebastiangolian@gmail.com>
2021-01-12 10:47:50 +0100

    modal-info - timeout

--- src/app/shared/components/modal-info/modal-info.component.ts
+++ src/app/shared/components/modal-info/modal-info.component.ts
@@ -4,21 +4,25 @@ import { Component, ChangeDetectionStrategy, Input, OnChanges } from '@angular/c
   selector: 'modal-info',
   templateUrl: './modal-info.component.html',
   styleUrls: ['./modal-info.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class ModalInfoComponent implements OnChanges {
   @Input() isCloseVisible = true;
   @Input() headerClass: string = '';
   @Input() datetime: string = '';
+  @Input() timeout: number = 0
 
-  public active: boolean = true;
+  active: boolean = true;
+
   constructor() {
     this.headerClass = "modal-header " + this.headerClass
+    this.active = true
   }
 
   ngOnChanges(): void {
     this.headerClass = "modal-header " + this.headerClass
     this.active = true
+    if (this.timeout > 0) setTimeout(() => { this.active = false }, this.timeout);
   }
 
   onClose(): void {
