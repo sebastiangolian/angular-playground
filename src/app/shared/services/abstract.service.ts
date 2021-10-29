3308ec67d968aea2e80428c0e906e9b570283da0
Sebastian Golian <sebastian.golian@solwit.com>
2021-10-29 15:55:35 +0200

    abstract.service - add pipe(first())

--- src/app/shared/services/abstract.service.ts
+++ src/app/shared/services/abstract.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { ApiList } from '../interfaces/api-list.interface';
 import { Observable } from 'rxjs/internal/Observable';
-import { Api } from '../interfaces/api.interface';
+import { first } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { ApiList } from '../interfaces/api-list.interface';
+import { Api } from '../interfaces/api.interface';
 
 export abstract class AbstractService<T> {
   url: string = environment.apiEndpoint;
@@ -31,34 +32,34 @@ export abstract class AbstractService<T> {
     if (parameters.length > 0) {
       url += '?' + parameters.join('&');
     }
-    return this.http.get<ApiList<T>>(url);
+    return this.http.get<ApiList<T>>(url).pipe(first());
   }
 
   getOne(): Observable<Api<T>> {
-    return this.http.get<Api<T>>(this.url);
+    return this.http.get<Api<T>>(this.url).pipe(first());
   }
 
   getById(id: string): Observable<Api<T>> {
-    return this.http.get<Api<T>>(this.url + '/' + id);
+    return this.http.get<Api<T>>(this.url + '/' + id).pipe(first());
   }
 
   post(item: T): Observable<T> {
     item = this.trimItem(item);
-    return this.http.post<T>(this.url, item);
+    return this.http.post<T>(this.url, item).pipe(first());
   }
 
   update(id: string, item: T): Observable<T> {
     item = this.trimItem(item);
-    return this.http.put<T>(this.url + '/' + id, item);
+    return this.http.put<T>(this.url + '/' + id, item).pipe(first());
   }
 
   patch(id: string, item: any): Observable<T> {
     item = this.trimItem(item);
-    return this.http.patch<T>(this.url + '/' + id, item);
+    return this.http.patch<T>(this.url + '/' + id, item).pipe(first());
   }
 
   delete(id: string): Observable<null> {
-    return this.http.delete<null>(this.url + '/' + id);
+    return this.http.delete<null>(this.url + '/' + id).pipe(first());
   }
 
   private trimItem(item: any): T {
