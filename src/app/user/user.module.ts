2efa1fb86ff69c1b8e09d7d2caa9f1fdfd6506ba
Sebastian Golian <sgolian@solwit.com>
2022-12-23 21:57:03 +0100

    update

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserRoutingModule } from './user-routing.module';
-import { UserDataTableComponent } from './pages/user-datatable/user-datatable.component';
-import { SharedModule } from '../shared/shared.module';
-import { UserModalComponent } from './components/user-modal/user-modal.component';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgxMaskModule } from 'ngx-mask';
-import { UserSearchComponent } from './pages/user-search/user-search.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
+import { SharedModule } from '../shared/shared.module';
+import { RoleModalSearchComponent } from './components/role-modal-search/role-modal-search.component';
+import { UserModalComponent } from './components/user-modal/user-modal.component';
+import { UserDataTableComponent } from './pages/user-datatable/user-datatable.component';
 import { UserIndexComponent } from './pages/user-index/user-index.component';
 import { UserItemComponent } from './pages/user-item/user-item.component';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { RoleModalSearchComponent } from './components/role-modal-search/role-modal-search.component';
+import { UserSearchComponent } from './pages/user-search/user-search.component';
+import { UserRoutingModule } from './user-routing.module';
 
 @NgModule({
   declarations: [
@@ -21,6 +21,14 @@ import { RoleModalSearchComponent } from './components/role-modal-search/role-mo
     UserModalComponent,
     RoleModalSearchComponent,
   ],
-  imports: [CommonModule, UserRoutingModule, SharedModule, FormsModule, NgxMaskModule.forRoot(), BsDropdownModule.forRoot()],
+  imports: [
+    CommonModule, 
+    UserRoutingModule, 
+    SharedModule, 
+    FormsModule, 
+    NgxMaskDirective, 
+    NgxMaskPipe,
+    BsDropdownModule.forRoot()
+  ],
 })
 export class UserModule {}
